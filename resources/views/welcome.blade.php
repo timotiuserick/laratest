3fc9561b350366cb6ada4e92bef5bd5fc5139cff
Timotius <timotiusericks@gmail.com>
2017-08-28 16:35:19 +0700

    fix errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel {{$user->number}}
                 </div>
 
                 <div class="links">
--- storage/framework/views/7c05d6463a3210f58b980394daca47378dd0b0c6.php
+++ storage/framework/views/7c05d6463a3210f58b980394daca47378dd0b0c6.php
@@ -79,7 +79,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel <?php echo e($user->number); ?>
+
                 </div>
 
                 <div class="links">
